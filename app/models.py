aff677496c137fcf51c413a983d6eb4cb8d001ff
kskorupka <kskorupka01@gmail.com>
2023-04-08 19:47:16 +0200

    add models

--- app/models.py
+++ app/models.py
@@ -22,3 +22,23 @@ class User(db.Model, fsqla.FsUserMixin):
         for attr in self.__mapper__.columns.keys():
             if attr in values:
                 yield attr, values[attr]
+
+
+class Offer(db.Model):
+    __tablename__ = 'offer'
+    __table_args__ = {'extend_existing': True}
+
+
+class Category(db.Model):
+    __tablename__ = 'category'
+    __table_args__ = {'extend_existing': True}
+
+
+class Order(db.Model):
+    __tablename__ = 'order'
+    __table_args__ = {'extend_existing': True}
+
+
+class UserScore(db.Model):
+    __tablename__ = 'user_score'
+    __table_args__ = {'extend_existing': True}
