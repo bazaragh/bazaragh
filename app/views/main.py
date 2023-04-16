dbbcc5b0159493044d0a07b86d31ee14df6417b7
HulekJakub <HulekJakub@users.noreply.github.com>
2023-04-16 23:41:49 +0200

    added offer form, fixed links, implemented basic offer creation

--- app/views/main.py
+++ app/views/main.py
@@ -8,6 +8,7 @@ from datetime import datetime
 from app.app import db
 from app.models import Category, Offer, User
 from flask_security import auth_required
+from app.forms.offer import AddEditOfferForm
 
 bp = Blueprint("bp_main", __name__)
 
@@ -44,15 +45,62 @@ def offer_get(offer_id):
                            images=images,
                            author=author)
 
-@bp.route('/user')
-def user_get():
-    return render_template('user/user.jinja')
+@bp.route('/offer/add')
+@auth_required()
+def offer_add():
+    form = AddEditOfferForm()
+    return render_template('offer/offer_add_edit.jinja', form=form, offer_id=None)
+
+@bp.route('/offer/<int:offer_id>/edit')
+@auth_required()
+def offer_edit(offer_id):
+    form = AddEditOfferForm()
+    return render_template('offer/offer_add_edit.jinja', form=form, offer_id=offer_id)
+
+# TODO ADD Post and Edit Put endpoints
+@bp.route('/api/offer/<int:offer_id>/edit', methods=["PUT"])
+@auth_required()
+def offer_edit_api(offer_id):
+    form = AddEditOfferForm()
+    return render_template('offer/offer_add_edit.jinja', form=form, method='PUT', offer_id=offer_id)
+
+@bp.route('/api/offer/add', methods=["POST"])
+@auth_required()
+def offer_add_api():
+    form = AddEditOfferForm()
+    offer = Offer(author=current_user.id,
+                  description=form.description.data,
+                  category=1,
+                  title=form.title.data,
+                  price=form.price.data,
+                  is_used=False,
+                  images='["1200px-RedCat_8727.jpg"]')
+    db.session.add(offer)   
+    db.session.commit()
+    return user_offers_get()
+# ======================================================
+
+@bp.route('/offer/<int:offer_id>/delete', methods=['DELETE'])
+@auth_required()
+def offer_delete(offer_id):
+    # TODO deletion
+    return user_offers_get()
+
+@bp.route('/user/profile')
+@auth_required()
+def user_profile_get():
+    return render_template('user/user_profile.jinja')
 
 @bp.route('/user/offers')
+@auth_required()
 def user_offers_get():
     offers = db.session.query(Offer).filter_by(author=current_user.id).all()
     return render_template('user/user_offers.jinja', offers=offers)
-@bp.route('/user-profile')
+
+@bp.route('/user/settings')
 @auth_required()
-def user_profile_get():
-    return render_template('user_profile.jinja')
+def user_settings_get():
+    return render_template('user/user_settings.jinja')
+
+# TODO settings PUT endpoint
+
