780ecaec7199a05d6f60e92d3dd56489d4e94e84
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2023-04-20 22:26:53 +0200

    #25 - Edycja darmowego ogłoszenia

--- app/forms/offer.py
+++ app/forms/offer.py
@@ -1,12 +1,15 @@
+import decimal
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, DecimalField, SubmitField
-from wtforms.validators import Length, DataRequired, NumberRange
+from wtforms.validators import Length, DataRequired, NumberRange, Optional
 
 
 class AddEditOfferForm(FlaskForm):
-    title = StringField('Tytuł', [DataRequired(message='Tytuł ogłoszenia jest wymagany'), Length(max=128)])
+    title = StringField('Tytuł', validators=[DataRequired(message='Tytuł ogłoszenia jest wymagany'), Length(max=128)])
     description = TextAreaField('Opis', [Length(max=4096)])
-    price = DecimalField('Cena', [NumberRange(min=0, max=None)])
+    price = DecimalField('Cena', validators=[Optional(), NumberRange(min=0, max=None)],
+                         description="Zostaw puste, by wystawić darmową ofertę", places=2, rounding=decimal.ROUND_UP)
     submit = SubmitField("Potwierdź")
 
 
