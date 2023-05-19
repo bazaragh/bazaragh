81f3591bb4a8ae4ff0f5103e9112bb373163c5af
Piotr Sokołowski <piotrsokolowski00@outlook.com>
2023-05-19 21:12:42 +0200

    Renamed functions

--- app/views/api.py
+++ app/views/api.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from flask_security import auth_required
 
 from app.app import db
 from app.models import Dormitory, Faculty
@@ -7,7 +8,7 @@ bp = Blueprint("bp_api", __name__, url_prefix="/api")
 
 
 @bp.route("/dormitories")
-def dormitories():
+def api_dormitories():
     dorms = db.session.query(Dormitory).all()
     dorms.sort(key=lambda x: int(x.id[2:].replace("DS", "0").strip()))
     results = [{"id": d.id, "text": f"{d.id} {d.name}"} for d in dorms]
@@ -15,7 +16,7 @@ def dormitories():
 
 
 @bp.route("/faculties")
-def faculties():
+def api_faculties():
     faculties = db.session.query(Faculty).all()
     results = [{"id": f.id, "text": f"W{f.id}"} for f in faculties]
     return jsonify({"results": results})
